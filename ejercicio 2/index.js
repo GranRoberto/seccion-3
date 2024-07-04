f56153ec7754ed3f3eff0767235199de35b8d95e
Roberto <robertocariolo94@gmail.com>
2024-07-04 17:26:05 -0400

    a

--- ejercicio 2/index.js
+++ ejercicio 2/index.js
@@ -5,15 +5,14 @@ let lista = document.getElementById('lista');
 let tarea = document.getElementById('tarea');
 let agregar = document.getElementById('agregar');
 
-// Función para agregar una nueva tarea
 function añadir() {
   let actividad = tarea.value.trim();
 
   if (actividad) {
     let lista_tareas = document.createElement('li');
     lista_tareas.innerHTML = `
-      <span>${actividad}</span>
       <input class="accent-blue-500 h-5 w-5 items-center" type="checkbox">
+      <span>${actividad}</span>
       <button class="eliminar">❌ Eliminar</button>
     `;
 
@@ -25,17 +24,14 @@ function añadir() {
   }
 }
 
-// Función para marcar una tarea como completada/incompleta
 function tarea_completada(event) {
   let lista_tareas = event.target.parentNode;
   lista_tareas.classList.toggle('completado');
 }
 
-// Función para eliminar una tarea
 function eliminar_tarea(event) {
   let lista_tareas = event.target.parentNode;
   lista.removeChild(lista_tareas);
 }
 
-// Agregar evento click al botón para agregar tareas
 agregar.addEventListener('click', añadir);
