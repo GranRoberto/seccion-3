6691449cffc086f9f9dd922c40c2e6d1b5747a24
Roberto <robertocariolo94@gmail.com>
2024-07-04 17:22:33 -0400

    a

--- ejercicio 2/index.js
+++ ejercicio 2/index.js
@@ -1,21 +1,41 @@
 /* 2) Crea un TO-DO list que permita agregar tareas, marcarlas como completadas y
-eliminarlas.
-var menu = "Bienvenido a la lista de tareas\n1. Agregar tarea\n2. Eliminar tarea\n3. Ver tareas\n";
-var tareas = [];
-var opcion = 0;
+eliminarlas.*/
 
-while (true) {
-  opcion = prompt(menu);
-  if (opcion === "1") {
-    var tarea = prompt("Escribe la tarea");
-    tareas.push(tarea);
-    alert("Tarea agregada");
-  } else if (opcion === "2") {
-    var indice = prompt("Escribe el número de la tarea que deseas eliminar");
-    tareas.splice(indice, 1);
-    alert("Tarea eliminada");
-  } else if (opcion === "3") {
-    alert(tareas);
+let lista = document.getElementById('lista');
+let tarea = document.getElementById('tarea');
+let agregar = document.getElementById('agregar');
+
+// Función para agregar una nueva tarea
+function añadir() {
+  let actividad = tarea.value.trim();
+
+  if (actividad) {
+    let lista_tareas = document.createElement('li');
+    lista_tareas.innerHTML = `
+      <span>${actividad}</span>
+      <input class="accent-blue-500 h-5 w-5 items-center" type="checkbox">
+      <button class="eliminar">❌ Eliminar</button>
+    `;
+
+    lista_tareas.addEventListener('click', tarea_completada);
+    lista_tareas.querySelector('.eliminar').addEventListener('click', eliminar_tarea);
+
+    lista.appendChild(lista_tareas);
+    tarea.value = '';
   }
-}*/
+}
 
+// Función para marcar una tarea como completada/incompleta
+function tarea_completada(event) {
+  let lista_tareas = event.target.parentNode;
+  lista_tareas.classList.toggle('completado');
+}
+
+// Función para eliminar una tarea
+function eliminar_tarea(event) {
+  let lista_tareas = event.target.parentNode;
+  lista.removeChild(lista_tareas);
+}
+
+// Agregar evento click al botón para agregar tareas
+agregar.addEventListener('click', añadir);
